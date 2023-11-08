b7357a550c96f30b3b17d24d08f8d003851db6bb
derya <derya.e1320@gmail.com>
2023-11-08 22:30:21 +0100

    third

--- userdata.sh
+++ userdata.sh
@@ -6,12 +6,12 @@ apt install python3-pip -y
 pip3 install boto3
 apt  install awscli -y
 cd /home/ubuntu/
-TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /osvaldo/capstone/token --with-decryption --query 'Parameter.Value' --output text)
-git clone https://$TOKEN@github.com/awsdevopsteam/capstone15.git
-cd /home/ubuntu/capstone15
+TOKEN=$(aws --region=us-east-1 ssm get-parameter --name /derya/capstone/token --with-decryption --query 'Parameter.Value' --output text)
+git clone https://$TOKEN@github.com/Derya-E/Capstone-Project-Django--on-AWS-Environment.git
+cd /home/ubuntu/Capstone-Project-Django--on-AWS-Environment
 apt-get install python3.10-dev default-libmysqlclient-dev -y
 pip3 install -r requirements.txt
-cd /home/ubuntu/capstone15/src
+cd /home/ubuntu/Capstone-Project-Django--on-AWS-Environment/src
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate
